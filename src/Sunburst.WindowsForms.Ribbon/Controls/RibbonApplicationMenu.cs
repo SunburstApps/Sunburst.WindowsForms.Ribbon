b04da5f11cd87b2d7da79ec5c4204fa99fb975f0
William Kent <wjk@users.noreply.github.com>
2018-02-12 10:47:34 -0500

    Add code to Sunburst.WindowsForms.Ribbon project

---
+++ src/Sunburst.WindowsForms.Ribbon/Controls/RibbonApplicationMenu.cs
@@ -0,0 +1,53 @@
+﻿//*****************************************************************************
+//
+//  File:       RibbonApplicationMenu.cs
+//
+//  Contents:   Helper class that wraps a ribbon application menu control.
+//
+//*****************************************************************************
+
+using Sunburst.WindowsForms.Ribbon.Controls.Properties;
+
+namespace Sunburst.WindowsForms.Ribbon.Controls
+{
+    public class RibbonApplicationMenu : BaseRibbonControl, 
+        ITooltipPropertiesProvider
+    {
+        private TooltipPropertiesProvider _tooltipPropertiesProvider;
+
+        public RibbonApplicationMenu(Ribbon ribbon, uint commandId)
+            : base(ribbon, commandId)
+        {
+            AddPropertiesProvider(_tooltipPropertiesProvider = new TooltipPropertiesProvider(ribbon, commandId));
+        }
+ 
+        #region ITooltipPropertiesProvider Members
+
+        public string TooltipTitle
+        {
+            get
+            {
+                return _tooltipPropertiesProvider.TooltipTitle;
+            }
+            set
+            {
+                _tooltipPropertiesProvider.TooltipTitle = value;
+            }
+        }
+
+        public string TooltipDescription
+        {
+            get
+            {
+                return _tooltipPropertiesProvider.TooltipDescription;
+            }
+            set
+            {
+                _tooltipPropertiesProvider.TooltipDescription = value;
+            }
+        }
+
+        #endregion
+
+    }
+}
